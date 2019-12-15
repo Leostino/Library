1419190e0b69a1b279bf559597540f4a52337f3a
Leonard Ezeudu <leonardezeudu@gmail.com>
2019-12-14 20:05:02 -0500

    moved models inside library folder and setup controller

--- library/routes/users.js
+++ library/routes/users.js
@@ -1,13 +1,51 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require("../models/index")
 
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Login');
 });
 
 router.get('/register', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Register');
+});
+
+router.post('/register', function(req, res) {
+
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  // check required fields
+
+  if ((!name) || (!email) || (!password) || (!password2)) {
+
+    errors.push({ msg: "Please fill in all fields"})
+  }
+
+  // check for passwords
+
+  if(password !== password2) {
+
+    errors.push({ msg: "Passwords do not match"})
+  }
+
+  // check length of password
+
+  if(password.length < 6) {
+
+    errors.push({ msg: "Password should be at least 6 characters"})
+  }
+
+  if(errors.length > 0) {
+
+    // render user registration
+
+  }else{
+
+    // create a user
+
+  }
 });
 
 module.exports = router;
