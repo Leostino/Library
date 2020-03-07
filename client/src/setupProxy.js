9a93aad3b6c4e072888a32c03e43a46edcbd37dc
Leonard Ezeudu <leonardezeudu@gmail.com>
2020-03-06 21:29:07 -0500

    added router to proxy obj

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,18 +1,22 @@
 const proxy = require('http-proxy-middleware');
 
     module.exports = function(app) {
-        app.use(proxy('/users/*', { target: 'https://poclibrary.herokuapp.com' ,
-        changeOrigin: true,
-              logLevel: 'debug',
-              router: {
-                'localhost:3000': 'http://localhost:8080'
-              }}));
-        app.use(proxy('/api/*', { target: 'https://poclibrary.herokuapp.com',
-        changeOrigin: true,
-              logLevel: 'debug',
-              router: {
-                'localhost:3000': 'http://localhost:8080'
-              }}));
+        app.use("/users/*", proxy({ 
+            target: 'https://poclibrary.herokuapp.com' ,
+            changeOrigin: true,
+            logLevel: 'debug',
+            router: {
+                'localhost:3000': 'http://localhost:8080/'
+            }
+        }));
+        app.use("/api/*", proxy({ 
+            target: 'https://poclibrary.herokuapp.com',
+            changeOrigin: true,
+            logLevel: 'debug',
+            router: {
+                'localhost:3000': 'http://localhost:8080/'
+            }
+        }));
     };
 
 
