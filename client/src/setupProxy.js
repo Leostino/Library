00edac6f12dd71af63eaa4290f500336fd5a32f3
Leonard Ezeudu <leonardezeudu@gmail.com>
2020-03-06 20:50:10 -0500

    changed proxy add to 127.0.0.1

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,18 @@
 const proxy = require('http-proxy-middleware');
 
     module.exports = function(app) {
-        app.use(proxy('/users/**', { target: 'http://localhost:8080/' }));
-        app.use(proxy('/api/**', { target: 'http://localhost:8080/' }));
+        app.use(proxy('/users/*', { target: 'http://127.0.0.1:8080/' }));
+        app.use(proxy('/api/*', { target: 'http://127.0.0.1:8080/' }));
     };
+
+
+    // module.exports = function(app) {
+    //     app.use('/api/*', proxy({
+    //       target: 'https://safe-crag-59591.herokuapp.com',
+    //       changeOrigin: true,
+    //       logLevel: 'debug',
+    //       router: {
+    //         'localhost:3000': 'http://localhost:3001'
+    //       }
+    //     }));
+    //   };
